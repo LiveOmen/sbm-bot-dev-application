51822c6441800c66b08028d4cb7d6a2addaa23c1
LiveOmen <88698683+LiveOmen@users.noreply.github.com>
2025-11-15 02:40:56 -0500

    Update register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -74,6 +74,121 @@ const commands = [
             },
         ],
     },
+    {
+        name: 'mute',
+        description: 'Mute a user',
+        options: [
+            {
+                name: 'user',
+                description: 'User to mute (if resolvable)',
+                type: ApplicationCommandOptionType.User,
+                required: false,
+            },
+            {
+                name: 'user-id',
+                description: 'ID of the user to mute',
+                type: ApplicationCommandOptionType.String,
+                required: false,
+            },
+            {
+                name: 'reason',
+                description: 'Reason for the mute',
+                type: ApplicationCommandOptionType.String,
+                required: false,
+            },
+            {
+                name: 'duration',
+                description: 'Duration of the mute (for temporary unmutes)',
+                type: ApplicationCommandOptionType.String,
+                required: false,
+            },
+        ],
+    },
+    {
+        name: 'unmute',
+        description: 'Unmute a user',
+        options: [
+            {
+                name: 'user',
+                description: 'User to unmute (if resolvable)',
+                type: ApplicationCommandOptionType.User,
+                required: false,
+            },
+            {
+                name: 'user-id',
+                description: 'ID of the user to unmute',
+                type: ApplicationCommandOptionType.String,
+                required: false,
+            },
+            {
+                name: 'reason',
+                description: 'Reason for the unmute',
+                type: ApplicationCommandOptionType.String,
+                required: false,
+            },
+        ],
+    },
+    {
+        name: 'automod',
+        description: 'Manage automod banned words',
+        options: [
+            {
+                type: ApplicationCommandOptionType.Subcommand,
+                name: 'add',
+                description: 'Add up to 3 words to the automod banned list',
+                options: [
+                    {
+                        name: 'word1',
+                        description: 'First word to add',
+                        type: ApplicationCommandOptionType.String,
+                        required: true,
+                    },
+                    {
+                        name: 'word2',
+                        description: 'Second word to add (optional)',
+                        type: ApplicationCommandOptionType.String,
+                        required: false,
+                    },
+                    {
+                        name: 'word3',
+                        description: 'Third word to add (optional)',
+                        type: ApplicationCommandOptionType.String,
+                        required: false,
+                    },
+                ],
+            },
+            {
+                type: ApplicationCommandOptionType.Subcommand,
+                name: 'remove',
+                description: 'Remove up to 3 words from the automod banned list',
+                options: [
+                    {
+                        name: 'word1',
+                        description: 'First word to remove',
+                        type: ApplicationCommandOptionType.String,
+                        required: true,
+                    },
+                    {
+                        name: 'word2',
+                        description: 'Second word to remove (optional)',
+                        type: ApplicationCommandOptionType.String,
+                        required: false,
+                    },
+                    {
+                        name: 'word3',
+                        description: 'Third word to remove (optional)',
+                        type: ApplicationCommandOptionType.String,
+                        required: false,
+                    },
+                ],
+            },
+            {
+                type: ApplicationCommandOptionType.Subcommand,
+                name: 'list',
+                description: 'List all automod banned words',
+            },
+        ],
+    },
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
